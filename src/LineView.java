b4c31939c43d79e8c56ce7c8a44f6cd535c067c9
Michael Clarke <mfc1@aber.ac.uk>
2014-06-09 15:53:15 +0100

    Pushed project files.

--- src/LineView.java
+++ src/LineView.java
@@ -1,4 +1,45 @@
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class LineView {
 
+public class LineView extends View {
+	
+	private String stationName = null;
+	
+	public LineView(Context context) {
+		super(context);
+	}
+	
+	public LineView(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+	}
+	
+	public LineView(Context context, AttributeSet attributeSet, int defaultStyle) {
+		super(context, attributeSet, defaultStyle);
+	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+	
+	protected void onDraw(Canvas canvas) {
+		
+		float radius;
+		if (this.getHeight() > this.getWidth()) {
+			radius = this.getWidth() / 2;
+		} else {
+			radius = this.getHeight() / 2;
+		}
+		
+		Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+		p.setStyle(Paint.Style.STROKE);
+		p.setStrokeWidth(30);
+		p.setColor(Color.RED);
+		canvas.drawCircle(this.getHeight()/2, this.getWidth()/2, radius, p);
+	}
+	
 }
