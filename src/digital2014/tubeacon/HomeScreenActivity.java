174c8bc6a98d85cd1d198cfab7723cc403c0ad59
ems <csemma@swan.ac.uk>
2014-06-10 10:08:35 +0100

    Fixed?

---
+++ src/digital2014/tubeacon/HomeScreenActivity.java
@@ -0,0 +1,37 @@
+package digital2014.tubeacon;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+
+public class HomeScreenActivity extends Activity {
+
+	private RouteItem currentLocation;
+	private LinearLayout ll;
+	private ArrayList<RouteItem> route;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home_screen);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.string.stations, android.R.layout.simple_spinner_item);
+
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+	]
+		
+	}
+	
+
+	
+	
+}
